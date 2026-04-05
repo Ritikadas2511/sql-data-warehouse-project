f0be63d0219580653ac5872777a21bf8a51a9613
Ritika <83827074+Ritikadas2511@users.noreply.github.com>
2026-04-06 00:08:24 +0530

    Update clean_transform.sql

--- scripts/silver/clean_transform.sql
+++ scripts/silver/clean_transform.sql
@@ -34,3 +34,34 @@ from bronze.crm_cust_info;
 Select distinct cst_marital_status
 from bronze.crm_cust_info;
 
+
+-- Check for Nulls or Duplicates in primary key column.
+
+Select 
+prd_key, count(*)
+from bronze.crm_prd_info
+group by prd_key
+having count(*)>1 or prd_key is null;
+
+-- Check for unwanted spaces in string columns.
+
+Select prd_nm
+from bronze.crm_prd_info
+where prd_nm != TRIM(prd_nm);
+
+-- Check for Nulls or Negative values.
+
+Select prd_cost
+from bronze.crm_prd_info
+where prd_cost<0 or prd_cost is null;
+
+-- Data standardization & consistency.
+
+Select distinct prd_line
+from bronze.crm_prd_info;
+
+-- Check for invalid date orders.
+
+Select *
+from bronze.crm_prd_info
+where prd_end_dt < prd_start_dt;
